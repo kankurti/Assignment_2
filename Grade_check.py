df4d764951d2662cfdaba0aabf2407b336ec11ef
kankurti <77320382+kankurti@users.noreply.github.com>
2025-10-26 23:56:00 +0530

    Add grade checking functionality based on marks

---
+++ Grade_check.py
@@ -0,0 +1,15 @@
+marks =int(input("Enter your marks"))
+
+if marks>=90:
+    print("A")
+elif marks>=80 and marks<90:
+    print("B")
+elif marks>=70 and marks<=79:
+    print("C")
+elif marks>=60 and marks<=69:
+    print("D")
+else:
+    print("F") 
+
+
+    
